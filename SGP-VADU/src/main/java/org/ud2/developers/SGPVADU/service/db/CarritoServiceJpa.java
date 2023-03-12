de6a0d428e3d149c05ce458104f47bd96358227b
Usiel06 <213241032@alumnos.utn.edu.mx>
2023-03-12 13:33:01 -0600

    sf

--- SGP-VADU/src/main/java/org/ud2/developers/SGPVADU/service/db/CarritoServiceJpa.java
+++ SGP-VADU/src/main/java/org/ud2/developers/SGPVADU/service/db/CarritoServiceJpa.java
@@ -3,10 +3,12 @@ package org.ud2.developers.SGPVADU.service.db;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.ud2.developers.SGPVADU.entity.Producto;
 import org.ud2.developers.SGPVADU.repository.CarritoRepository;
 import org.ud2.developers.SGPVADU.service.IntServiceCarrito;
 
+@Service
 public class CarritoServiceJpa implements IntServiceCarrito {
 
 	@Autowired
