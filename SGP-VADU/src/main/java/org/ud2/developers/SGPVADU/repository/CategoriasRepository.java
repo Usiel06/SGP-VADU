8bade9fbc51b5f6c0bcc3eeb78349807158252d5
Usiel Colin <127615655+Usiel06@users.noreply.github.com>
2023-03-24 18:28:48 -0600

    Delete SGP-VADU directory

--- SGP-VADU/src/main/java/org/ud2/developers/SGPVADU/repository/CategoriasRepository.java
+++
@@ -1,8 +0,0 @@
-package org.ud2.developers.SGPVADU.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.ud2.developers.SGPVADU.entity.Categoria;
-
-public interface CategoriasRepository extends JpaRepository<Categoria, Integer> {
-	
-}
--- SGP-VADU/src/main/java/org/ud2/developers/SGPVADU/repository/ProductosRepository.java
+++
@@ -1,8 +0,0 @@
-package org.ud2.developers.SGPVADU.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.ud2.developers.SGPVADU.entity.Producto;
-
-public interface ProductosRepository extends JpaRepository<Producto, Integer> {
-
-}
